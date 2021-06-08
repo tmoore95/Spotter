8157430f12988a7338ad74d00f8074918cbfe24d
Patrick Hoveman <patchinator@hotmail.co.uk>
2021-06-08 18:25:13 +0100

    changes to embed function

--- app/javascript/plugins/init_get_post.js
+++ app/javascript/plugins/init_get_post.js
@@ -1,17 +1,15 @@
-var POST_URL = document.querySelector("form-control string url required")
-var post = document.querySelector(".post")
+const POST_URL = document.getElementById("tag_url")
+const APP_ID = "517239189416893"
+const CLIENT_TOKEN = "82dfc1d6a7fb3105b7f341b6629fe36a"
 
-async function getPost() {
-  var response = await fetch(
+async function initGetPost() {
+  const response = await fetch(
     "https://graph.facebook.com/v8.0/instagram_oembed?url=" +
     POST_URL + "&omitscript=true&access_token=" + APP_ID + "|" +
     CLIENT_TOKEN)
-  var data = await response.json()
-  var myPostHtml = data.html
-  post.innerHTML = myPostHtml
-  instgrm.Embeds.process()
+  const data = await response.json()
+  console.log(data)
+  const myPostHtml = data.html
 }
 
-export { getPost };
-
-/* <div class="post"></div> */
+export { initGetPost };
