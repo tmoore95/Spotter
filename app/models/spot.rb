a700d94d935db1639990a830bb2dbb1ef871e832
Tom Moore <tom_moore95@hotmail.co.uk>
2021-06-09 17:57:41 +0100

    added dependent destroy for checkins

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,7 +1,7 @@
 class Spot < ApplicationRecord
   belongs_to :skater
   has_many :tags, dependent: :destroy
-  has_many :check_ins
+  has_many :check_ins, dependent: :destroy
   has_many_attached :photos
   validates :name, presence: true, length: { in: 3..30 }
   acts_as_favoritable
