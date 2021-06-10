d49cd259e47f7e7d37f0a7aa15f92a564c1dfb47
hrwndavies <hrwndavies@gmail.com>
2021-06-10 16:36:04 +0100

    Profile page favorite spots

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,14 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_skater!, only: :home
   def home
   end
+
+  def profile
+   @skater = Skater.find(params[:id])
+   @current_skater = current_skater
+  end
+
+  def toggle_favorite
+    @skater = Skater.find(params[:id])
+    current_skater.favorited?(@skater) ? current_skater.unfavorite(@skater) : current_skater.favorite(@skater)
+  end
 end
