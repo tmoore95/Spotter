936a67cd1dbb922bf486a48547fe2f25b0dbd7d3
Patrick Hoveman <patchinator@hotmail.co.uk>
2021-06-10 14:19:40 +0100

    changed margins to makes videos smaller

--- app/javascript/plugins/init_get_post.js
+++ app/javascript/plugins/init_get_post.js
@@ -4,15 +4,14 @@ const post = document.querySelector(".post");
 
 const initGetPost = () => {
   const urls = document.querySelectorAll(".urlbb");
-  urls.forEach((url) => {
+  urls.forEach((url) => { 
    getPost(url)
-    console.log(url.textContent)
   })
 }
 
 let getPost = async function (url) {
   var response = await fetch(
-    `http://iframe.ly/api/iframely?url=${url.textContent}&api_key=` + process.env.["FRAMELY_API_KEY"])
+    `http://iframe.ly/api/iframely?url=${url.textContent}&api_key=${process.env.["FRAMELY_API_KEY"]}`)
   var data = await response.json()
   var myPostHtml = data.html
   url.nextElementSibling.innerHTML = myPostHtml
