d0696dadc2896cec7d4aca2f7e5d0cd45ee0c3a1
Tom Moore <tom_moore95@hotmail.co.uk>
2021-06-16 17:23:03 +0100

    added notification pop up for followers and moved filter button

--- app/javascript/components/refresh.js
+++ app/javascript/components/refresh.js
@@ -3,91 +3,101 @@ import Rails from "@rails/ujs"
 let state = false
 const refresh = () => {
   
-  setInterval(function () {
-    Rails.ajax({
-      url: '/feed?ajax=1', type: "get", success: function (data) {
-        let updatedFeed = data.querySelector(".feed").innerHTML;
-        const feed = document.querySelector(".feed")
-        if (feed) {
-          feed.innerHTML = updatedFeed;
-        }
-        const count = updatedFeed.split("feed-card").length - 1
-        const seen = parseInt(document.cookie.split("=")[1])
-        const notif = document.getElementById("notif")
-        if (count > seen) {
-          notif.classList.remove("d-none")
-          const total = count - seen
-          notif.innerHTML = total
-        }
-      }
-    });
-  }, 5000);
-};
-//    --- WORK IN PROGRESS FOR FOLLOWER NOTIFICATIONS AS WELL ---
-
 //   setInterval(function () {
-//     Rails.ajax({ url: '/feed?ajax=1', type: "get", success: function(data) { 
-//       let updatedFeed = data.querySelector(".feed").innerHTML;
-//       const feed = document.querySelector(".feed")
-//       if (feed) {
-//         feed.innerHTML = updatedFeed;
-//       }
-//       const count = updatedFeed.split("feed-card").length - 1
-//       let seen = document.cookie.split(";");
-//       console.log(seen)
-//       let seenFollowers = 0;
-//       let seenFeed = 0;
-//       if (seen.length === 2) 
-//       // if (seen[0] === "") {
-//       //   seen = ["followers=0", "activity=0", true];
-//       // } else if (seen.length === 1 && seen[0].charAt[0] === "f") {
-//       //   seenFollowers = seen[0].split("=")[1];
-//       // } else if (seen.length === 1 && seen[0].charAt[0] === "a") {
-//       //   seenFeed = seen[0].split("=")[1];
-//       // } else if (seen.length === 2 && seen[0].charAt[0] === "f") {
-//       //   seen = [array[0].trim(), array[1].trim()]
-//       //   seenFollowers = seen[0].split("=")[1];
-//       //   seenFeed = seen[1].split("=")[1];
-//       // } else {
-//       //   seen = [array[0].trim(), array[1].trim()]
-//       //   seenFollowers = seen[1].split("=")[1];
-//       //   seenFeed = seen[0].split("=")[1];
-//       // }
-//       console.log(seen)
-//       console.log(seenFollowers)
-//       console.log(seenFeed)
-
-//       const notif = document.getElementById("notif")
-//       if (count > seenFeed) {
-//         notif.classList.remove("d-none")
-//         const total = count - seen
-//         notif.innerHTML = total
-//       }
-//       const skaterId = document.querySelector(".navbar").getAttribute("data-id");
-//       Rails.ajax({ url: `/skaters/${skaterId}?ajax=1`, type: 'get', success: function(data) {
-//         const nodeList = data.querySelectorAll(".follower_username");
-//         let followers_array = Array.prototype.slice.call(nodeList);
-//         followers_array = followers_array.map(element => element.innerHTML);
-//         const new_follower = followers_array[followers_array.length - 1];
-//         let updatedFollowers = data.querySelector(".followers").innerHTML;
-//         const followers = document.querySelector(".followers");
-//         if (followers) {
-//           followers.innerHTML = updatedFollowers;
+//     Rails.ajax({
+//       url: '/feed?ajax=1', type: "get", success: function (data) {
+//         let updatedFeed = data.querySelector(".feed").innerHTML;
+//         const feed = document.querySelector(".feed")
+//         if (feed) {
+//           feed.innerHTML = updatedFeed;
 //         }
-//         const followerCount = updatedFollowers.split("follow-user").length - 1
-//         const follower_notif = document.querySelector(".follower_notif")
-//         if (seen[3] === null) {
-//           if (followerCount > seenFollowers && state === false) {
-//             follower_notif.innerHTML = `${new_follower} started following you!`
-//             follower_notif.classList.remove("d-none")
-//             state = true
-//             setTimeout(() => follower_notif.remove(), 5000)
-//           }
+//         const count = updatedFeed.split("feed-card").length - 1
+//         const seen = parseInt(document.cookie.split("=")[1])
+//         const notif = document.getElementById("notif")
+//         console.log(count)
+//         console.log(seen)
+//         console.log(document.cookie)
+//         if (count > seen) {
+//           notif.classList.remove("d-none")
+//           const total = count - seen
+//           notif.innerHTML = total
 //         }
 //       }
-//     })
-//     }});
+//     });
 //   }, 5000);
 // };
+//    --- WORK IN PROGRESS FOR FOLLOWER NOTIFICATIONS AS WELL ---
+
+  setInterval(function () {
+    Rails.ajax({ url: '/feed?ajax=1', type: "get", success: function(data) { 
+      let updatedFeed = data.querySelector(".feed").innerHTML;
+      const feed = document.querySelector(".feed")
+      if (feed) {
+        feed.innerHTML = updatedFeed;
+      }
+      const count = updatedFeed.split("feed-card").length - 1
+      let seen = document.cookie.split(";");
+      let seenFollowers = 0;
+      let seenFeed = 0;
+      if (seen.length === 2) {
+        seen = [seen[0].trim(), seen[1].trim()]
+      }
+      if (seen[0] === "") {
+        seen = ["followers=0", "activity=0", true];
+      }
+      if (seen.length === 1 && seen[0].charAt(0) === "f") {
+        seenFollowers = seen[0].split("=")[1];
+      }
+      if (seen.length === 1 && seen[0].charAt(0) === "a") {
+        seenFeed = seen[0].split("=")[1];
+      }
+      if (seen.length === 2 && seen[0].charAt(0) === "f") {       
+        seenFollowers = seen[0].split("=")[1];
+        seenFeed = seen[1].split("=")[1];
+      }
+      if (seen.length === 2 && seen[0].charAt(0) === "a") {
+        seenFollowers = seen[1].split("=")[1];
+        seenFeed = seen[0].split("=")[1];
+      }
+      console.log(seen )
+      console.log(`seen followers = ${seenFollowers}`)
+      console.log(`seen feed = ${seenFeed}`)
+      console.log(`updated feed count = ${count}`)
+
+      const notif = document.getElementById("notif")
+      if (count > seenFeed) {
+        notif.classList.remove("d-none")
+        const total = count - seenFeed
+        notif.innerHTML = total
+      }
+      const skaterId = document.querySelector(".navbar").getAttribute("data-id");
+      console.log(skaterId)
+      Rails.ajax({ url: `/skaters/${skaterId}?ajax=1`, type: 'get', success: function(data) {
+        const nodeList = data.querySelectorAll(".follower_username");
+        let followers_array = Array.prototype.slice.call(nodeList);
+        followers_array = followers_array.map(element => element.innerHTML);
+        const new_follower = followers_array[followers_array.length - 1];
+        let updatedFollowers = data.querySelector(".followers").innerHTML;
+        const followers = document.querySelector(".followers");
+        if (followers) {
+          followers.innerHTML = updatedFollowers;
+        }
+        const followerCount = updatedFollowers.split("follow-user").length - 1
+        console.log(`follower count${followerCount}`)
+        const follower_notif = document.querySelector(".follower_notif")
+        console.log(seen[3])
+        if (seen[3] === undefined || seen[3] === null) {
+          if (followerCount > seenFollowers && state === false) {
+            follower_notif.innerHTML = `🔔 ${new_follower} started following you!`
+            follower_notif.classList.remove("d-none")
+            state = true
+            setTimeout(() => follower_notif.remove(), 100000)
+          }
+        }
+      }
+    })
+    }});
+  }, 5000);
+};
 
 export { refresh };
